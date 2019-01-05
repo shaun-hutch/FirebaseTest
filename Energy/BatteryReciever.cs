a3ea063043177a6cd73aa882a18754ac5ca42e8b
Shaun Hutchinson <shutchnz@gmail.com>
2019-01-05 17:26:55 +1300

    Added battery broadcast receiver and intent filter.

---
+++ Energy/BatteryReciever.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace Energy
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    public class BatteryReciever : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            int batteryLevel = intent.GetIntExtra(BatteryManager.ExtraLevel, -1);
+            Toast.MakeText(context, $"Battery Level: {batteryLevel}%", ToastLength.Long);
+            Console.WriteLine(batteryLevel);
+        }
+    }
+}
--- Energy/MainActivity.cs
+++ Energy/MainActivity.cs
@@ -1,19 +1,50 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using System;
 
 namespace Energy
 {
     [Activity(Label = "Energy", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : Activity 
     {
+        private TextView txtBatteryLevel;
+        int batteryLevel;
+        int batteryScale;
+        int batteryPercentage;
+        BatteryReciever receiver;
+        IntentFilter filter;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            receiver = new BatteryReciever();
+            filter = new IntentFilter(Intent.ActionBatteryChanged);
+            var battery = RegisterReceiver(receiver, filter);
+            txtBatteryLevel = FindViewById<TextView>(Resource.Id.batteryLevel);
+            batteryLevel = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+            batteryScale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+            batteryPercentage = int.Parse(Math.Floor(batteryLevel * 100D / batteryScale).ToString());
+
+            txtBatteryLevel.Text = $"Battery Level: {batteryPercentage}%";
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(receiver, filter);
+            Console.WriteLine("registered!");
+        }
+
+        protected override void OnStop()
+        {
+            UnregisterReceiver(receiver);
+            Console.WriteLine("Unregistered!");
+            base.OnStop();
+        }
+
+
     }
 }
-
