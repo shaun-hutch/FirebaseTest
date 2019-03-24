e66336689434198400bb4c24cfae7e04f9fc99d3
Shaun Hutchinson <shutchnz@gmail.com>
2019-03-24 20:33:55 +1300

    Added basic authentication from boiler plate code online.

---
+++ FirebaseTest/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirebaseTest.Entities;
+using FirebaseTest.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirebaseTest.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]User userParam)
+        {
+            var user = await _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ FirebaseTest/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using FirebaseTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirebaseTest.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+    }
+
+    public class UserService : IUserService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
+        };
+
+        //basic authentication, no security at all
+        public async Task<User> Authenticate(string username, string password)
+        {
+            var user = await Task.Run(() => _users.SingleOrDefault(x => x.Username == username && x.Password == password));
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so return user details without password
+            user.Password = null;
+            user.Token = new Guid();
+            user.TokenExpiry = DateTime.Now.AddSeconds(3600);
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            // return users without passwords
+            return await Task.Run(() => _users.Select(x => {
+                x.Password = null;
+                return x;
+            }));
+        }
+    }
+}
